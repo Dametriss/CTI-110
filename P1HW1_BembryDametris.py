d75203cf5a87f7a8f575b055e9c9a4cd34eb621f
Dametriss <146122442+Dametriss@users.noreply.github.com>
2023-09-26 14:21:47 -0400

    Add files via upload

---
+++ P1HW1_BembryDametris.py
@@ -0,0 +1,11 @@
+#Dametris Bembry
+#9/26/23
+#Input, Output, Expressions
+
+num1 = int(input("Enter a number: "))
+print("You entered: ", num1)
+print(num1, "squared is", num1**2)
+print("And", num1, "cubed is ", num1**3)
+num2= int(input("Enter another number: "))
+print(num1, "+", num2, "=", num1+num2)
+print(num1, "*", num2, "=", num1*num2)
